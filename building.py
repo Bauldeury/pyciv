b21eede204b41d3ad9fb3b2900509a0110831904
Bauldeury <67470810+Bauldeury@users.noreply.github.com>
2022-02-17 13:54:28 +0100

    building: change
    using a dictionnary now on
    choose to fit on CIV1 ruleset

--- building.py
+++ building.py
@@ -1,8 +1,21 @@
-keys = {"PALACE","CASERN", "LIBRARY", "UNIVERSITY", "GRANARY"}
-    
-# def validateKey(key):
-    # if key not in building.keys:
-        # raise Exception("ERROR: BuildingKeyUnknown")
+class _building:
+    def __init__(self):
+        self.name = "Unnamed Building"
+        self.hammerCost = 50
+        self.purchaseCost = 150
+        self.maintenance = 1
+
+#keys = {"AQUEDUCT","BANK","BARRACKS","CATHEDRAL","CITY WALLS","COLOSSEUM","COURTHOUSE","FACTORY","GRANARY","HYDRO PLANT", "LIBRARY", "MARKETPLACE","MASS TRANSIT","MFG PLANT", "NUCLEAR PLANT", "PALACE", "POWER PLANT", "RECYCLING CENTER", "SDI DEFENSE","TEMPLE","UNIVERSITY"}
+_dico = {}
+
+def _loadDictionnary():
+
+    barracks = _building()
+    barracks.name = "barracks"
+    barracks.hammerCost = 40
+    barracks.purchaseCost = 160
+    barracks.maintenance = 0
+    _dico["BARRACKS"] = barracks
 
 def getFoodProduction(key_set):
     output = 0
@@ -10,6 +23,14 @@ def getFoodProduction(key_set):
         output += 5
     return output
     
-def getBuildables(key_set):
-    output = keys - key_set - {"PALACE"}
+def getGoldProduction(key_set):
+    #maintenance of the buildings
+    output = 0
     return output
+    
+def getBuildables(key_set):
+    output = set()
+    
+    return output
+
+_loadDictionnary()
