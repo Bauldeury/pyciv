594beab68eb84141bb5c2e4f46213bd40d1cb045
Bauldeury <67470810+Bauldeury@users.noreply.github.com>
2022-02-27 23:44:02 +0100

    added player, todolist
    added player
    added todolist

---
+++ player.py
@@ -0,0 +1,15 @@
+class player:
+    def __init__(self):
+        self.key = 0
+        self.name = "CivName"
+        self.leaderName = "LeaderName"
+        self.adjective = "civnamian"
+        self.color = "FF0000"
+        self.color2 = "FFFF00"
+        
+        self.techs = set()
+        self.gold = 100
+    
+    def __repr__(self):
+        return "C_PLAYER#{}".format(self.key)
+        
