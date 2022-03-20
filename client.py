9d34b3979bb8c6706273d6f4511bee9f6082d8d6
Bauldeury <67470810+Bauldeury@users.noreply.github.com>
2022-03-20 21:40:58 +0100

    network work

--- client.py
+++ client.py
@@ -1,5 +1,71 @@
-class client:
+import socket
+import select
+import tkinter as tk
+
+class clientApp(tk.Tk):
+    def __init__(self):
+        tk.Tk.__init__(self)
+        self.title("Client V2")
+        
+        # self.bind('<Escape>',self.stop)
+        self.protocol("WM_DELETE_WINDOW", self.stop)
+        
+        self._initWidgets()
+        self._connect()
+        
+
+    def _initWidgets(self):  
+        self.geometry("400x300")
+        self.config(padx = 15,pady = 10)
     
+        self.m_chatView = tk.Message(self, text="lorem", relief=tk.SUNKEN, bg = "WHITE")
+        self.e_chatEntry = tk.Entry(self)
+        self.b_send = tk.Button(self, text="envoyer", command=self.press_button)
+
+        self.m_chatView.pack(expand=True, fill='both')
+        self.e_chatEntry.pack(side=tk.LEFT,expand=True, fill = 'x')
+        self.b_send.pack(side=tk.RIGHT)
+        
+        
+    def _connect(self):
+        self.hote = "localhost"
+        self.port = 6951
+        
+        self.ssocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.ssocket.connect((self.hote, self.port))
+        print ("[+] Connection on {}:{}.".format(self.hote,self.port))
+        
+    def start(self):
+        self.quitflag = False
+        while True:
+            self.update()
+            if self.quitflag:
+                break
+                
+            # self.ssocket.setblocking(0)
+            rsock, wsock, esock = select.select([self.ssocket],[],[],0.05)
+            for sock in rsock:
+                response = sock.recv(256).decode()
+                if response != b"":
+                    print(response)
+                    self.printChat(response)
+            
+    def stop(self):
+        self.quitflag = True
+        self.ssocket.close()
+        print ("[-] Connection closed.")     
+        
+    def press_button(self):
+        print("button pressed!")
+        message = self.e_chatEntry.get()
+        if message != "":
+            self.e_chatEntry.delete(0,tk.END)
+            self.ssocket.send(message.encode())
+            
+    def printChat(self,message):
+        self.m_chatView.config(text=message)
+
+        
     def listenInfo(self):
         pass
         
@@ -8,3 +74,7 @@ class client:
         
     def sendCmd(self,cmd):
         pass
+        
+if __name__ == "__main__":
+    app = clientApp()
+    app.start()
--- server.py
+++ server.py
@@ -1,68 +1,103 @@
+import socket
+import threading
+
 class server:
+
     def __init__(self):
-        self.players = set()
+        self.connectionThreads = set()
         self.listening = False
+        
+        self.sock = socket.socket()
+        host = ""
+        port = 6951
+        self.sock.bind((host,port))
         
     def start(self):
         self.listening = True
-        self.listenForConnection()
+        self._listenForConnection()
     
     def stop(self):
         self.listening = False
-        for x in self.players:
+        for x in self.connectionThreads:
             x.stop()
+        self.sock.close()
         
     def __del__(self):
         self.stop()
         
-    def listenForConnection(self):
-        while self.listening:
-            break
-            #listen conn
-            #open conn
-            conn = playerConnection(self)
-            addConnection(conn)
-            
-        pass
         
-    def addConnection(self, conn):
-        self.players.add(conn)
-        conn.start()
-    
-    def removeConnection(self, conn):
-        conn.stop()
-        self.players.remove(conn)
+    def _listenForConnection(self):
+        while self.listening:
+            self.sock.listen(5)
+            (conn, (c_ip, c_port)) = self.sock.accept()
+            
+            print ("{}:{} opened connection".format(c_ip, c_port))
+            
+            #open conn
+            thread = connectionThread(self,conn,c_ip,c_port)
+            self.connectionThreads.add(thread)
+            thread.start()
+        self.stop()
+       
+       
+    def removeConnectionThread(self, conn):
+        # conn.stop()
+        self.connectionThreads.remove(conn)
 
         
     def broadcastInfo(self,info):
-        for x in self.players:
+        for x in self.connectionThreads:
             x.sendInfo(info)
 
 
     
-class playerConnection:
-    def __init__(self,server):
+class connectionThread(threading.Thread):
+    def __init__(self,server,conn,ip,port):
         self.server = server
+        self.conn = conn
+        self.ip = ip
+        self.port = port
+        
+        print("[+] Nouveau thread pour {}:{}".format(self.ip, self.port))
         
     def start(self):
         #ask authentification (civkey)
         #receive auth
         self.civkey = "TODO"
-        #setup playerConnection
-        #setup thread
-        pass
-    
-    def stop(self):
-        pass
+        #setup connectionThread
         
-    def stopCmd(self):
-        server.removeConnection(self)
-        
-    def listenCmd(self):
-        pass
+        msg = ""
+   
+        while True:
+            try:
+                message = self.conn.recv(256)
+                if message:
+                    self.executeCmd(message)
+                else:
+                    print("clean disconnect [type 1]")
+                    break
+            except:
+                print("connection interrupted [type 2]")
+                break 
+
     
     def executeCmd(self,cmd):
-        pass
+        print(cmd.decode())
         
     def sendInfo(self, info):
-        pass
+        try:
+            conn.send(message)
+        except:
+            conn.close()
+            print("connection interrupted [type 3]")
+            remove(conn)
+
+    def stop(self,msg = "No msg"):
+        self.conn.close()
+        print("[-] Fin du thread pour {}:{}".format(self.ip, self.port))
+        server.removeConnectionThread(self)
+        
+                
+if __name__ == "__main__":
+    s = server()
+    s.start()
