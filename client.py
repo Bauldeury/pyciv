24a769f61420d9cfa0e45a22a4d4da60734d6338
Bauldeury <67470810+Bauldeury@users.noreply.github.com>
2022-03-24 08:24:59 +0100

    Update client.py

--- client.py
+++ client.py
@@ -15,16 +15,25 @@ class clientApp(tk.Tk):
         
 
     def _initWidgets(self):  
-        self.geometry("400x300")
-        self.config(padx = 15,pady = 10)
+        self.geometry("800x600")
+        self.config(padx = 5,pady = 5)
     
-        self.m_chatView = tk.Message(self, text="lorem", relief=tk.SUNKEN, bg = "WHITE")
-        self.e_chatEntry = tk.Entry(self)
-        self.b_send = tk.Button(self, text="envoyer", command=self.press_button)
+        self.f_main = tk.Frame(self)
+        self.f_main.config(bg="RED", relief=tk.SUNKEN)
+        self.f_main.pack(side=tk.LEFT,padx = 5,pady = 5, expand=True,fill = 'both')
+        
+        self.f_details = tk.Frame(self)
+        self.f_details.config(bg="BLUE", relief=tk.SUNKEN)
+    
+        self.m_chatView = tk.Message(self.f_details, text="lorem", relief=tk.SUNKEN, bg = "WHITE")
+        self.e_chatEntry = tk.Entry(self.f_details)
+        self.b_send = tk.Button(self.f_details, text="envoyer", command=self.press_button)
 
         self.m_chatView.pack(expand=True, fill='both')
         self.e_chatEntry.pack(side=tk.LEFT,expand=True, fill = 'x')
         self.b_send.pack(side=tk.RIGHT)
+        
+        self.f_details.pack(side=tk.LEFT,padx = 5,pady = 5, expand=False,fill = 'both')
         
         
     def _connect(self):
@@ -67,7 +76,7 @@ class clientApp(tk.Tk):
         message = self.e_chatEntry.get()
         if message != "":
             self.e_chatEntry.delete(0,tk.END)
-            sendCmd(message)
+            self.sendCmd(message)
             
     def printChat(self,message):
         self.m_chatView.config(text=message)
