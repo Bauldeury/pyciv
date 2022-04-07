81d3ce685e903f3297a350dfc100920131db75fe
Bauldeury <67470810+Bauldeury@users.noreply.github.com>
2022-04-07 19:28:33 +0200

    chisme interface/networking

--- client/client.py
+++ client/client.py
@@ -1,41 +1,16 @@
 import socket
 import select
-import tkinter as tk
 
-class clientApp(tk.Tk):
+from mainPanel import mainPanel
+
+class clientApp():
     def __init__(self):
-        tk.Tk.__init__(self)
-        self.title("Client V2")
+        self.pMain = mainPanel(self)
+        self.pMain.protocol("WM_DELETE_WINDOW", self.stop)
         
-        # self.bind('<Escape>',self.stop)
-        self.protocol("WM_DELETE_WINDOW", self.stop)
-        
-        self._initWidgets()
         self._connect()
         
-
-    def _initWidgets(self):  
-        self.geometry("800x600")
-        self.config(padx = 5,pady = 5)
-    
-        self.f_main = tk.Frame(self)
-        self.f_main.config(bg="RED", relief=tk.SUNKEN)
-        self.f_main.pack(side=tk.LEFT,padx = 5,pady = 5, expand=True,fill = 'both')
-        
-        self.f_details = tk.Frame(self)
-        self.f_details.config(bg="BLUE", relief=tk.SUNKEN)
-    
-        self.m_chatView = tk.Message(self.f_details, text="lorem", relief=tk.SUNKEN, bg = "WHITE")
-        self.e_chatEntry = tk.Entry(self.f_details)
-        self.b_send = tk.Button(self.f_details, text="envoyer", command=self.press_button)
-
-        self.m_chatView.pack(expand=True, fill='both')
-        self.e_chatEntry.pack(side=tk.LEFT,expand=True, fill = 'x')
-        self.b_send.pack(side=tk.RIGHT)
-        
-        self.f_details.pack(side=tk.LEFT,padx = 5,pady = 5, expand=False,fill = 'both')
-        
-        
+ 
     def _connect(self):
         self.hote = "localhost"
         self.port = 6951
@@ -49,10 +24,12 @@ class clientApp(tk.Tk):
         self.ssocket.close()
         print ("[-] Connection closed.")   
         
+        
     def start(self):
         self.quitflag = False
+
         while True:
-            self.update()
+            self.pMain.update()
             if self.quitflag:
                 break
                 
@@ -65,21 +42,11 @@ class clientApp(tk.Tk):
   
     def executeInfo(self,info):
         print(info)
-        self.printChat(info)
+        self.pMain.printChat(info)
         
     def sendCmd(self,cmd):
         self.ssocket.send(cmd.encode())
-        
-        
-    def press_button(self):
-        print("button pressed!")
-        message = self.e_chatEntry.get()
-        if message != "":
-            self.e_chatEntry.delete(0,tk.END)
-            self.sendCmd(message)
-            
-    def printChat(self,message):
-        self.m_chatView.config(text=message)
+
 
         
 
---
+++ client/mainPanel.py
@@ -0,0 +1,46 @@
+import tkinter as tk
+
+class mainPanel(tk.Tk):
+    def __init__(self,client):
+        tk.Tk.__init__(self)
+        
+        self.title("Client V2")
+        self.client = client
+        
+        self._initWidgets()
+        
+    def update(self):
+        tk.Tk.update(self)
+
+    def _initWidgets(self):  
+        self.geometry("800x600")
+        self.config(padx = 5,pady = 5)
+    
+        self.f_main = tk.Frame(self)
+        self.f_main.config(bg="RED", relief=tk.SUNKEN)
+        self.f_main.pack(side=tk.LEFT,padx = 5,pady = 5, expand=True,fill = 'both')
+        
+        self.f_details = tk.Frame(self)
+        self.f_details.config(bg="BLUE", relief=tk.SUNKEN)
+    
+        self.m_chatView = tk.Message(self.f_details, text="lorem", relief=tk.SUNKEN, bg = "WHITE")
+        self.e_chatEntry = tk.Entry(self.f_details)
+        self.b_send = tk.Button(self.f_details, text="envoyer", command=self.press_button)
+
+        self.m_chatView.pack(expand=True, fill='both')
+        self.e_chatEntry.pack(side=tk.LEFT,expand=True, fill = 'x')
+        self.b_send.pack(side=tk.RIGHT)
+        
+        self.f_details.pack(side=tk.LEFT,padx = 5,pady = 5, expand=False,fill = 'both')
+        
+                
+        
+    def press_button(self):
+        print("button pressed!")
+        message = self.e_chatEntry.get()
+        if message != "":
+            self.e_chatEntry.delete(0,tk.END)
+            self.client.sendCmd(message)
+            
+    def printChat(self,message):
+        self.m_chatView.config(text=message)
